fe241dd628e6d0dc1464b52a890d154342a8a714
ninak235 <116868070+ninak235@users.noreply.github.com>
2023-03-09 17:13:08 +0100

    views created

---
+++ InitialProject/InitialProject/View/Guest1MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using InitialProject.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace InitialProject.View
+{
+    /// <summary>
+    /// Interaction logic for Guest1MainWindow.xaml
+    /// </summary>
+    public partial class Guest1MainWindow : Window
+    {
+        public Guest1MainWindow(User user)
+        {
+            InitializeComponent();
+        }
+    }
+}
